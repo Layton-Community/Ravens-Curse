668a1843a7bb717ef2e208b3698d132926c0bb5f
Phnix <76911907+MrPhnix@users.noreply.github.com>
2022-07-27 15:02:48 +0200

    refactor: moving GDScript project
    The GDScript project will be moved to a new branch.

--- Game/Characters/Character.gd
+++
@@ -1,7 +0,0 @@
-extends Node2D
-
-
-onready var image = $Sprite
-
-func flip():
-	image.flip_h = true
--- Game/GLOBAL.gd
+++
@@ -1,9 +0,0 @@
-extends Node
-
-var current_puzzle_times_wrong = 0
-var picarats = 0
-var puzzle_max_picarats = {"001": 20, "002": 40}
-var puzzle_number_times_wrong = {"001": 0, "002": 0}
-var current_puzzle = "002"
-var puzzle_correct = false
-var language = "es"
