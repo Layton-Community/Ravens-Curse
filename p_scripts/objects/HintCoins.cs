e45a5e778cf7030194df27c4412727879eda9358
MrPhnix <76911907+MrPhnix@users.noreply.github.com>
2023-08-14 23:23:02 +0200

    Add hint coins

---
+++ p_scripts/objects/HintCoins.cs
@@ -0,0 +1,44 @@
+namespace Com.LaytonCommunity.RavensCurse.Game;
+
+[GlobalClass]
+[Icon("res://a_sprites/objects/hint_coin.png")]
+public partial class HintCoins : Control
+{
+	// Constants
+	private const string ANIM_SPIN = "spin";
+	public const string GROUP = "coins";
+	
+	// Enums
+	
+	// Signal
+	[Signal] public delegate void CollectedEventHandler();
+	
+	// Export variables
+	[Export] private TextureButton button;
+	[Export] private AnimationPlayer animations;
+
+	// Member variables
+
+	public override void _Ready()
+	{
+		Modulate = Colors.Transparent;
+		
+		button.Pressed += OnButton_Pressed;
+	}
+
+	private void OnButton_Pressed()
+	{
+		if (animations != null && !animations.IsPlaying())
+		{
+			Modulate = Colors.White;
+			
+			animations.AnimationFinished += (_) =>
+			{
+				QueueFree();
+			};
+			
+			animations.Play(ANIM_SPIN);
+		}
+	}
+}
+
